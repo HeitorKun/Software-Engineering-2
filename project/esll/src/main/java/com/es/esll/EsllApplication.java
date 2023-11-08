bf2aa859a0f687ec1bbf00792b44d4a2fef46c1e
juAlberti <julia.maia@acad.pucrs.br>
2023-11-07 22:26:42 -0300

    adicionando base de codigo dos adapters

--- project/esll/src/main/java/com/es/esll/EsllApplication.java
+++ project/esll/src/main/java/com/es/esll/EsllApplication.java
@@ -2,8 +2,12 @@ package com.es.esll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.es" })
+@EntityScan(basePackages = { "com.es" })
 public class EsllApplication {
 
 	public static void main(String[] args) {
---
+++ project/esll/src/main/java/com/es/esll/adapters/controllers/DataBase_Service/User.java
@@ -0,0 +1,11 @@
+package main.java.com.es.esll.adapters.controllers.DataBase_Service;
+
+public class User {
+    String username;
+    String password;
+    
+    public Student(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
